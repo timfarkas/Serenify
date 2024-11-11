413df32092e831fe06c94a4cc7b9146e44565196
Todd Taylor <toddtaylor@eduroam-int-dhcp-97-44-9.ucl.ac.uk>
2024-11-11 11:05:26 +0000

    logout page made and oop made

--- login/logout.py
+++ login/logout.py
@@ -1,15 +1,36 @@
 import tkinter as tk
-import 
+import subprocess
 
-# Create the main window
-root = tk.Tk()
-root.title("Logout")
+class LogoutPage:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Logout")
+        self.root.geometry("400x300")
 
-h1_label = tk.Label(root, text="Logout successful!", font=("Arial", 24, "bold"))
-h1_label.pack()
+        h1_label = tk.Label(root, text="Logout successful!", font=("Arial", 24, "bold"))
+        h1_label.pack()
 
-# Login button
-login_button = tk.Button(root, text="Log back in", command=root.login.py)
-login_button.pack()
+        # # Login button
+        # login_button = tk.Button(root, text="Log back in", command=self.login.py)
+        # login_button.pack()
 
-root.mainloop()
+        # Login button
+        self.login_button = tk.Button(root, text="Login", command=self.loginButton)
+        self.login_button.pack()
+
+    def exitUser(self):
+        pass
+        ######### Inputs #########
+        # username = self.username_entry.get()
+        # password = self.password_entry.get()
+        # role = self.user_role.get()
+
+    def loginButton(self):
+        subprocess.Popen(["python3", "login/login.py"])
+        self.root.destroy()
+
+# Run the application
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = LogoutPage(root)
+    root.mainloop()
