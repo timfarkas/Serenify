5fbcfd312be85182c3d431821630a71a7225977e
Todd Taylor <toddtaylor@eduroam-int-dhcp-97-44-9.ucl.ac.uk>
2024-11-12 15:48:28 +0000

    Building on frontend python

--- login/resetPassword.py
+++ login/resetPassword.py
@@ -14,20 +14,31 @@ class ResetPage:
         fieldset = tk.LabelFrame(root, text="Please enter your email", padx=10, pady=10)
         fieldset.pack(padx=10, pady=10)
 
-        name_label = tk.Label(fieldset, text="Email:")
-        name_label.grid(row=0, column=0)
-        name_entry = tk.Entry(fieldset)
-        name_entry.grid(row=0, column=1)
+        self.name_label = tk.Label(fieldset, text="Email:")
+        self.name_label.grid(row=0, column=0)
+        self.name_entry = tk.Entry(fieldset)
+        self.name_entry.grid(row=0, column=1)
 
         # Reset button
         self.reset_button = tk.Button(root, text="Reset", command=self.resetButton)
         self.reset_button.pack()
+    
+    def validateEmail(self):
+        checkEmail = self.name_entry.get() # Collect users email entered in fieldset
+        if self.correctDetails(checkEmail): # This must be changed to fit with database data
+            self.resetButton()
+        else:
+            messagebox.showerror("Login Failed", "Invalid username or password")
+
+    def correctDetails(self):
+        if self.name_entry == 'example123@gmail.com':
+            print("This email is in our database! Sending you a password reset link now")
+            return True
+
 
     def resetButton(self):
-
-        pass
-        # subprocess.Popen(["python3", "login.py"])
-        # self.root.destroy()
+        subprocess.Popen(["python3", "login.py"])
+        self.root.destroy()
 
 # Run the application
 if __name__ == "__main__":
